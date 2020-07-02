831238559e236edf3d64a936bd71b143c1199d3e
Hussein Talal <hossientalal2@gmail.com>
2020-07-02 22:50:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution here
 function findMatching(drivers, str){
-  let result = drivers.filter(driver => driver === str)
+  let result = drivers.filter(driver => driver.toLowercase() === str.toLowercase())
   return result
 }
