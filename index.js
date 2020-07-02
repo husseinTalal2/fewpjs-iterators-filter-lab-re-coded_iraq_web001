ad6ee416ad03cd44f284dd35fdcd179c5d72f1fe
Hussein Talal <hossientalal2@gmail.com>
2020-07-02 22:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function findMatching(drivers, str){
   let result = drivers.filter(driver => driver.toLowerCase() === str.toLowerCase())
   return result
 }
+function fuzzyMatch(drivers, str){
+  let result = drivers.filter(driver => driver === str)
+  return result
+}
